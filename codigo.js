a4935b1231eb61919ed24a5b7ecc3adf98f9ed47
JoseDeLaCruz28 <1josemaria09luis@gmail.com>
2023-10-06 17:50:18 -0500

    cambios 7

--- codigo.js
+++ codigo.js
@@ -7,3 +7,24 @@ btnsesion.addEventListener("click", e =>{
     formRegistro.classList.add("hide");
     formSesion.classList.remove("hide")
 })
+
+/*btnregistrar.addEventListener("click", e =>{
+    formSesion.classList.add("hide");
+    formRegistro.classList.remove("hide");
+})
+
+
+const btnRecuperaC = document.getElementById("recuperar");
+    btnRegresar = document.getElementById("Regresa");
+    formrecuperando = document.querySelector(".recuperando");
+    formRegistro = document.querySelector(".registro");
+
+btnRecuperaC.addEventListener("click", e=>{
+    formrecuperando.classList.add("hide2");
+    formRegistro.classList.remove("hide2");
+})
+btnRegresar.addEventListener("click", e=>{
+    formRegistro.classList.add("hide2");
+    formrecuperando.classList.remove("hide2");
+})
+*/
