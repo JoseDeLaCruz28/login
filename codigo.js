cca57c91ec1b0fb2909a04d5fa18ae60df1ca2aa
JoseDeLaCruz28 <1josemaria09luis@gmail.com>
2023-10-06 11:45:42 -0500

    cambios 6

--- codigo.js
+++ codigo.js
@@ -1,2 +1,9 @@
 const btnsesion = document.getElementById("sign-in");
-      btnregistrar = document.getElementById("sign-up"); 
+      btnregistrar = document.getElementById("sign-up");
+      formRegistro = document.querySelector(".registro");
+      formSesion = document.querySelector(".Sesion");
+
+btnsesion.addEventListener("click", e =>{
+    formRegistro.classList.add("hide");
+    formSesion.classList.remove("hide")
+})
