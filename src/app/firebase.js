79825ab91c385bea7821fff9c286d9f8c8bdec9b
JoseDeLaCruz28 <1josemaria09luis@gmail.com>
2023-10-13 17:31:59 -0500

    Nuevos cambios Viernes

---
+++ src/app/firebase.js
@@ -0,0 +1,18 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+    
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAKZAziYPkH_gl-dZtlA7UyHUO09MINAHg",
+    authDomain: "fir-app-tutu-8984c.firebaseapp.com",
+    projectId: "fir-app-tutu-8984c",
+    storageBucket: "fir-app-tutu-8984c.appspot.com",
+    messagingSenderId: "509111706447",
+    appId: "1:509111706447:web:e83e5aac75680e3c779b3a"
+  };
+    
+  // Initialize Firebase
+  export const app = initializeApp(firebaseConfig);
+  
---
+++ src/codigo.js
@@ -0,0 +1,14 @@
+const btnsesion = document.getElementById("sign-in");
+      btnregistrar = document.getElementById("sign-up");
+      formRegistro = document.querySelector(".registro");
+      formSesion = document.querySelector(".Sesion");
+
+btnsesion.addEventListener("click", e =>{
+    formRegistro.classList.add("hide");
+    formSesion.classList.remove("hide")
+})
+
+btnregistrar.addEventListener("click", e =>{
+    formSesion.classList.add("hide");
+    formRegistro.classList.remove("hide")
+})
